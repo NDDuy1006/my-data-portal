86ea4980b1ff463cfaf775639c6160d3eeb4d9c9
Duy ND <duynd1006@gmail.com>
2024-02-16 10:05:35 +0700

    finished implemented open api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Realtor App')
+    .setDescription('The Realtor API description')
+    .setVersion('1.0')
+    .addTag('realtor')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
--- src/user/auth/dtos/UserAuthPayload.ts
+++ src/user/auth/dtos/UserAuthPayload.ts
@@ -8,26 +8,32 @@ import {
   Matches,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserAuthPayload {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({ type: 'string' })
   email: string;
 
   @IsNotEmpty()
   @MinLength(5)
+  @ApiProperty({ type: 'string' })
   password: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ type: 'string' })
   firstname: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ type: 'string' })
   lastname: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ type: 'string' })
   @Matches(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/, {
     message: 'Phone must be a valid phone number',
   })
@@ -35,23 +41,28 @@ export class UserAuthPayload {
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ type: 'string' })
   productKey?: string;
 }
 
 export class UserSigninPayload {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
 
 export class GenerateProductKeyPayload {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsEnum(UserType)
+  @ApiProperty()
   userType: UserType;
 }
